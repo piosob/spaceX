2fa6cac834b82e27b5f50e22dfa04507be8ebfe6
Piotr Sobieski <psobieski4@gmail.com>
2022-05-15 17:23:40 +0200

    done nav

--- src/components/PageHeader/PageHeader.js
+++ src/components/PageHeader/PageHeader.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import './PageHeader.scss'
+import HeaderCSS from './PageHeader.module.scss'
 
 export const PageHeader = () => {
   return (
-    <header className='page-header'>
-      <nav className='page-header__nav'>
-        <ul className='page-header__ul'>
-          <li><NavLink to="/" end className="home-selected">home</NavLink></li>
-          <li><NavLink to="/latest-flight" className="latest-flight-selected" >latest-flight</NavLink></li>
-          <li><NavLink to="/missions" className="missions-selected" ></NavLink>missions</li>
-          <li><NavLink to="/about" className="about-selected" >about</NavLink></li>
-        </ul>
-      </nav>
+    <header className={HeaderCSS.header}>
+      <div className={HeaderCSS.container}>
+        <nav className={HeaderCSS.nav}>
+          <ul className={HeaderCSS.ul}>
+            <div className="row">
+              <div className="col-3"><li className={HeaderCSS.li}><NavLink className={HeaderCSS.link} to="/" end >home</NavLink></li></div>
+              <div className="col-3"><li className={HeaderCSS.li}><NavLink className={HeaderCSS.link} to="/latest-flight" >latest-flight</NavLink></li></div>
+              <div className="col-3"><li className={HeaderCSS.li}><NavLink className={HeaderCSS.link} to="/missions">missions</NavLink></li></div>
+              <div className="col-3"><li className={HeaderCSS.li}><NavLink className={HeaderCSS.link} to="/about">about</NavLink></li></div>
+            </div>
+          </ul>
+        </nav>
+
+
+      </div>
     </header>
   );
 }
