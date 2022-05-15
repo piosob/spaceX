e72358ad31a3a6946ed194b8d772d6eca98a2541
Piotr Sobieski <psobieski4@gmail.com>
2022-05-15 19:55:51 +0200

    route rocket: added params

--- src/pages/rockets/Rocket.js
+++ src/pages/rockets/Rocket.js
@@ -5,11 +5,38 @@ export const Rocket = ({ name, desc, firstFl, height, diameter, mass, imgs, cost
     <div className={styles.card}>
       <h3 className={styles.heading}>{name}</h3>
       <p className={styles.desc}>{desc}</p>
+      <div className={styles.paramsContainer}>
+        <div className={styles.paramsCard}>
+          <h4 className={styles.paramsHeading}>
+            First Flight:
+          </h4>
+          <p className={styles.paramsDesc}>{firstFl}</p>
+        </div>
+        <div className={styles.paramsCard}>
+          <h4 className={styles.paramsHeading}>
+            Height:
+          </h4>
+          <p className={styles.paramsDesc}>{height} m</p>
+        </div>
+        <div className={styles.paramsCard}>
+          <h4 className={styles.paramsHeading}>
+            Diameter:
+          </h4>
+          <p className={styles.paramsDesc}>{diameter} m</p>
+        </div>
+        <div className={styles.paramsCard}>
+          <h4 className={styles.paramsHeading}>
+            Mass:
+          </h4>
+          <p className={styles.paramsDesc}>{mass} kg</p>
+        </div>
+      </div>
       <div className={styles.imgContainer}>
         {imgs.map(img => (
           <img key={img} src={img} className={styles.img} alt="amazing SpaceX rocket" />
         ))}
       </div>
-    </div>
+
+    </div >
   );
 }
