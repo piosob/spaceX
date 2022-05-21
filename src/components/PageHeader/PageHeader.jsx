b0017aac385e9ef725ef65235d48a90f5179ec9e
Piotr Sobieski <psobieski4@gmail.com>
2022-05-21 20:25:11 +0200

    PageHeader -> change height onScroll

--- src/components/PageHeader/PageHeader.jsx
+++ src/components/PageHeader/PageHeader.jsx
@@ -1,25 +1,77 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
 import { NavLink } from 'react-router-dom';
-import HeaderCSS from './PageHeader.module.scss'
-import '../../scss/index.scss'
+import HeaderCSS from './PageHeader.module.scss';
+import '../../scss/index.scss';
 
 export const PageHeader = () => {
+  const [navShow, setNavShow] = useState(false);
+  const controlNavbar = () => {
+    if (window.scrollY > 100) {
+      setNavShow(true);
+    } else {
+      setNavShow(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', controlNavbar);
+    return () => {
+      window.removeEventListener('scroll', controlNavbar);
+    };
+  }, []);
+
   return (
-    <header className={HeaderCSS.header}>
+    <header className={navShow ? HeaderCSS.headerNarrower : HeaderCSS.headerWider}>
       <div className={HeaderCSS.container}>
         <nav className={HeaderCSS.nav}>
           <ul className={HeaderCSS.ul}>
             <div className="row">
-              <div className="col-3"><li className={HeaderCSS.li}><NavLink className={HeaderCSS.link} to="/" end >home</NavLink></li></div>
-              <div className="col-3"><li className={HeaderCSS.li}><NavLink className={HeaderCSS.link} to="/rockets" >rockets</NavLink></li></div>
-              <div className="col-3"><li className={HeaderCSS.li}><NavLink className={HeaderCSS.link} to="/missions">launches</NavLink></li></div>
-              <div className="col-3"><li className={HeaderCSS.li}><NavLink className={HeaderCSS.link} to="/about">about</NavLink></li></div>
+              <div className="col-3">
+                <li className={HeaderCSS.li}>
+                  <NavLink
+                    className={navShow ? HeaderCSS.linkNarrower : HeaderCSS.linkWider}
+                    to="/"
+                    end
+                  >
+                    home
+                  </NavLink>
+                </li>
+              </div>
+              <div className="col-3">
+                <li className={HeaderCSS.li}>
+                  <NavLink
+                    className={navShow ? HeaderCSS.linkNarrower : HeaderCSS.linkWider}
+                    to="/rockets"
+                  >
+                    rockets
+                  </NavLink>
+                </li>
+              </div>
+              <div className="col-3">
+                <li className={HeaderCSS.li}>
+                  <NavLink
+                    className={navShow ? HeaderCSS.linkNarrower : HeaderCSS.linkWider}
+                    to="/missions"
+                  >
+                    launches
+                  </NavLink>
+                </li>
+              </div>
+              <div className="col-3">
+                <li className={HeaderCSS.li}>
+                  <NavLink
+                    className={navShow ? HeaderCSS.linkNarrower : HeaderCSS.linkWider}
+                    to="/about"
+                  >
+                    about
+                  </NavLink>
+                </li>
+              </div>
             </div>
           </ul>
         </nav>
-
-
       </div>
     </header>
   );
-}
+};
